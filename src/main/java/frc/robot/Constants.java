bd95379718e0058e3a7e53b6c2a9885b72588f96
El1zabethS <Bestlemon555@gmail.com>
2024-07-31 19:22:52 -0700

    Created framework for Repository

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,74 @@
+// Copyright 2021-2024 FRC 6328
+// http://github.com/Mechanical-Advantage
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// version 3 as published by the Free Software Foundation or
+// available in the root directory of this project.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.RobotBase;
+import java.util.Optional;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+  /** Defines State of Robot */
+  public static class RobotStateConstants {
+    public static enum Mode {
+      /** Running on a real robot. */
+      REAL,
+
+      /** Running a physics simulator. */
+      SIM,
+
+      /** Replaying from a log file. */
+      REPLAY
+    }
+
+    /** Gets Robot Mode (Real, Sim, or Replay) */
+    public static final Mode getMode() {
+      if (RobotBase.isReal()) {
+        return Mode.REAL;
+      } else if (RobotBase.isSimulation()) {
+        return Mode.SIM;
+      } else {
+        return Mode.REPLAY;
+      }
+    }
+
+    /** Get Alliance (Blue, Red, Null) */
+    public static final Optional<Alliance> getAlliance() {
+      return DriverStation.getAlliance();
+    }
+
+    /** If CAN takes too long, it cancels */
+    public static final int CAN_CONFIG_TIMEOUT_SEC = 30;
+
+    /** Command Loop Seconds */
+    public static final double LOOP_PERIODIC_SEC = 0.02;
+
+    /** Average Battery Voltage */
+    public static final double BATTERY_VOLTAGE = 12;
+  }
+
+  /** Defines all Operater Constants */
+  public static final class OperatorConstants {
+    public static final int DRIVER_PORT = 0;
+    public static final int AUX_PORT = 1;
+  }
+}
