222b4c644203e9e51335d619daf3bf7d31a1b4ee
BranchProductivity <branchproductivity@gmail.com>
2024-11-11 20:12:28 -0700

    LimeLight

--- src/main/java/frc/robot/utils/LimeLight.java
+++ src/main/java/frc/robot/utils/LimeLight.java
@@ -4,10 +4,19 @@
 
 package frc.robot.utils;
 
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.estimator.PoseEstimator;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.drive.ModuleIOInputsAutoLogged;
 import frc.robot.subsystems.gyro.Gyro;
+import frc.robot.LimelightHelpers;
+import frc.robot.LimelightHelpers.PoseEstimate;
+
+import java.lang.Math;
+
+
+import org.opencv.core.Mat;
 
 /** Add your docs here. */
 public class LimeLight extends SubsystemBase {
@@ -15,10 +24,22 @@ public class LimeLight extends SubsystemBase {
     private ModuleIOInputsAutoLogged inputs = new ModuleIOInputsAutoLogged();
     private final Gyro gyro;
 
+    
+    private double getYaw() {
+        return Math.atan2(gyro.getYaw().getCos(), gyro.getYaw().getSin());
+    }
+    
+    private void setRobotOrientation(double Yaw) {
+    double robotYaw = Yaw;
 
-    public Rotation2d getYaw() {
-        return gyro.getYaw();
-           }
+    LimelightHelpers.SetRobotOrientation("", robotYaw, 0.0, 0.0, 0.0, 0.0, 0.0);
+
+    LimelightHelpers.PoseEstimate limelightMeasurement = LimelightHelpers.getBotPoseEstimate_wpiBlue("");
+
+    PoseEstimator.(VecBuilder.fill(.5, .5, 9999999));
+    }
+
+
 
 
 
