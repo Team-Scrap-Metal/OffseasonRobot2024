3e5f6d57206d554fb25da20af57a95cda6d03f59
El1zabethS <Elizabeth.Seaton35@gmail.com>
2024-11-25 21:10:15 -0700

    WIP

--- src/main/java/frc/robot/commands/LimelightTestCommands/DriveToVisionTarget.java
+++ src/main/java/frc/robot/commands/LimelightTestCommands/DriveToVisionTarget.java
@@ -9,14 +9,20 @@ import frc.robot.subsystems.drive.Drive;
 
 public class DriveToVisionTarget extends Command {
   /** Creates a new DriveToVisionTarget. */
-  public DriveToVisionTarget(Drive drive) {
+  private Drive drive;
+  private double distance;
+  public DriveToVisionTarget(Drive drive, double distance) {
     // Use addRequirements() here to declare subsystem dependencies.
+    this.drive = drive;
+    this.distance = distance;
     addRequirements(drive);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    drive.setRaw(0,0,0);
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
