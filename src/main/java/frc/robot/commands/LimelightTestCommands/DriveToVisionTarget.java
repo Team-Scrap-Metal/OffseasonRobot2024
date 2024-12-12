22f92fb19e3f4c7bd58321ca096ea30da9c82000
El1zabethS <Bestlemon555@gmail.com>
2024-12-11 18:04:56 -0700

    Quick WIP version of code for wednesday testing

--- src/main/java/frc/robot/commands/LimelightTestCommands/DriveToVisionTarget.java
+++ src/main/java/frc/robot/commands/LimelightTestCommands/DriveToVisionTarget.java
@@ -6,11 +6,17 @@ package frc.robot.commands.LimelightTestCommands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.drive.Drive;
+import frc.robot.utils.LimelightHelpers;
+import frc.robot.utils.LimelightHelpers.LimelightResults;
 
 public class DriveToVisionTarget extends Command {
   /** Creates a new DriveToVisionTarget. */
   private Drive drive;
   private double distance;
+  private double rot = 0;
+  private double x = 0;
+  private double y = 0;
+
   public DriveToVisionTarget(Drive drive, double distance) {
     // Use addRequirements() here to declare subsystem dependencies.
     this.drive = drive;
@@ -26,11 +32,43 @@ public class DriveToVisionTarget extends Command {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    if(LimelightHelpers.getTX("limelight") < -0.5){
+      rot = 2;
+    } else if (LimelightHelpers.getTX("limelight") > 0.5){
+      rot = -2;
+    } else {
+      rot = 0;
+    double distanceX = LimelightHelpers.getTargetPose3d_RobotSpace("limelight").getX();
+    double distanceY = LimelightHelpers.getTargetPose3d_RobotSpace("limelight").getY();
+    
+    if(distanceX > 0.5){
+      x = 2;
+    } else if (distanceX < -0.5){
+      x = -2;
+    } else {
+      x = 0;
+    }
+
+    if(distanceY > 0.5){
+      y = 2;
+    } else if (distanceY < -0.5){
+      y = -2;
+    } else {
+      y = 0;
+    }
+    }
+
+    
+
+    drive.setRaw(x, y, rot);
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    drive.setRaw(0,0,0);
+  }
 
   // Returns true when the command should end.
   @Override
